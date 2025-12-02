5aa2333fb548e522ec34f19e5e5bc039d722c566
Harrek <sergiomm.like@gmail.com>
2025-12-02 10:41:50 -0600

    Changed OG and removed default images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,7 +141,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/spiritbloompro-social-card.jpg',
+      image: 'img/Spiritbloom.png',
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
